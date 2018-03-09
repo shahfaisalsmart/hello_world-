458d9c9b1a48d85b7d4409ba38d067280a2ded04
shahfaisalsmart <shahfaisalsmart1234@gmail.com>
2018-03-09 15:32:14 +0530

    Create Breaking the Records.c

---
+++ Breaking the Records.c
@@ -0,0 +1,33 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+
+
+#define INFINITY 100000001
+
+int main() 
+{
+    int no_of_games, current_score, minimum = INFINITY, maximum = -1, minimum_changes = -1, maximum_changes = -1, i; 
+    scanf("%d",&no_of_games);
+    for(i = 1; i <= no_of_games; i++)
+    {
+        scanf("%d", &current_score);
+
+        if(current_score > maximum)
+        {
+            maximum = current_score; maximum_changes++; 
+        }
+        if(current_score < minimum)
+        {
+            minimum = current_score; minimum_changes++;
+        }
+    }
+
+    printf("%d %d\n",maximum_changes, minimum_changes);
+    return 0;
+}
